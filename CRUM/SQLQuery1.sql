4b27d80456b0f45af137f6f6d815ded6fbaaaddd
monicalesseur <monicalesseur@gmail.com>
2022-06-29 07:01:48 -0500

    commit

---
+++ CRUM/SQLQuery1.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE clients (
+    id INT NOT NULL PRIMARY KEY IDENTITY,
+    name VARCHAR (100) NOT NULL,
+    email VARCHAR (150) NOT NULL UNIQUE,
+    phone VARCHAR(20) NULL,
+    address VARCHAR(100) NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO clients (name, email, phone, address)
+VALUES
+('John Wilson', 'john@gmail.com', '+7215735721', 'Iowa, USA'),
+('Juan Pérez', 'juano@gmail.com', '+4426374839', 'Querétaro, México'),
+('Lee Minho', 'minho@gmail.com', '+81725715372', 'Seúl, South Korea'),
+('Walter Smith', 'walsmi@gmail.com', '+111555999', 'Colorado, USA'),
+('Jovani R', 'jovar@gmail.com', '+63286721573', 'Madrid, España'),
+('Brayan H', 'elbrayan@gmail.com', '+4426373859', 'Tepito, México');
